c707d294d6b95155f7b9a726e3c0879c22b0766b
Miquel Salva <msalva@inforolot.com>
2023-05-31 18:26:40 +0200

    Proves mapa

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
+import { ConnectWallet, Web3Button } from "@thirdweb-dev/react";
+import { useContract, useTotalCount, useNFTs } from "@thirdweb-dev/react";
+import { Card, Image, CardBody, CardFooter, Heading, Text, Stack, Divider, ButtonGroup, Button, SimpleGrid, Box, Center } from '@chakra-ui/react'
+// import "./styles/Home.css";
 
 export default function Home() {
+
+  const { contract } = useContract("0xcD6fFa6362b161a8B1F8698Af6E5e3531Ffc03D6")
+  const { data: count, isLoading, error } = useTotalCount(contract);
+  const { data: nfts, isLoading: loading, error: err } = useNFTs(contract, { start: 0, count: 100 });
+
+
+  
   return (
     <div className="container">
       <main className="main">
@@ -18,6 +27,61 @@ export default function Home() {
         <div className="connect">
           <ConnectWallet dropdownPosition={{ side: 'bottom', align: 'center' }} />
         </div>
+        <div>
+          <Web3Button
+            contractAddress="0xcD6fFa6362b161a8B1F8698Af6E5e3531Ffc03D6"
+            action={async (contract) => contract.call("totalSupply")}
+          >
+            Call totalSupply from the connected wallet
+          </Web3Button>
+        </div>
+        <div>{isLoading ? <p>Loading...</p> : <p>Contract Name: {count?.toString()}</p>}</div>
+        
+        <SimpleGrid columns={4} spacing={10}>
+          {nfts?.map((nft)=>
+            (
+              
+              <Card align={"center"} border={"2px"} borderColor={"white"}>
+                <CardBody >
+                  <Image
+                    src={nft.metadata.image!} 
+                    alt=""
+                    borderRadius='lg'
+                    boxSize={'300px'}
+                    objectFit="contain"
+                    align={"center"}
+                  />
+                  <Stack 
+                    mt='2' 
+                    spacing='1' 
+                    align='center'
+                    textAlign='center'>
+                    <Heading size='md'>{nft.metadata.name!}</Heading>
+                    <Text w={"300px"}>
+                      {nft.metadata.description!} 
+                    </Text>
+                    <Text color='blue.600' fontSize='2xl'>
+                      $450
+                    </Text>
+                  </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                  <ButtonGroup spacing='2'>
+                    <Button variant='solid' colorScheme='blue'>
+                      Buy now
+                    </Button>
+                    <Button variant='ghost' colorScheme='blue'>
+                      Add to cart
+                    </Button>
+                  </ButtonGroup>
+                </CardFooter>
+              </Card>
+
+            ))}
+              </SimpleGrid>
+
+
 
         <div className="grid">
           <a href="https://portal.thirdweb.com/" className="card">
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import Map from "./Map";
 import reportWebVitals from "./reportWebVitals";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "./styles/globals.css";
@@ -8,15 +9,15 @@ import "./styles/globals.css";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const activeChain = "mumbai";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
-      <App />
-    </ThirdwebProvider>
+    {/* <ThirdwebProvider activeChain={activeChain}> */}
+      <Map />
+    {/* </ThirdwebProvider> */}
   </React.StrictMode>
 );
 
