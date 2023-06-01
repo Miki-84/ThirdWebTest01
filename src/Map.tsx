b9750ad6a45d67d7c348a52bc04de2a946371f3d
Miquel Salva <msalva@inforolot.com>
2023-06-01 13:01:07 +0200

    kind of working

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { LatLngBoundsExpression } from 'leaflet';
 import { MapContainer, TileLayer, CircleMarker, Rectangle } from 'react-leaflet'
 
@@ -6,22 +7,55 @@ import * as L from 'leaflet';
 
 export default function Map() {
 
+  const [itemsList, setItemsList] = useState<string[][]>()
+  const [lands, setLands] = useState<React.ReactNode[]>() 
+
   const rectangle = [[50,50], [108, 170]] as LatLngBoundsExpression;
   const maxMapBounds = [[0,0], [108, 192]] as LatLngBoundsExpression;
 
   const orange = { color: 'orange' }
   const purpler = { color: 'purple'}
 
-  const lands: React.ReactNode[] = []
+  const localLands: React.ReactNode[] = []
+  var items: string[][]
+  // showFile = async (e) => {
+  //   e.preventDefault()
+  //   const reader = new FileReader()
+  //   reader.onload = async (e) => { 
+  //     const text = (e.target.result)
+  //     console.log(text)
+  //     alert(text)
+  //   };
+  //   reader.readAsText(e.target.files[0])
+  // }
+  useEffect(() => {
+    console.log("useeffect 1")
+    fetch('./test.txt').then(function (response) {
+      response.text().then(function (text) {
+        items = text.split("\r\n").map(function (el) { return el.split(" "); });
+        setItemsList(items)
+        getLands()
+        console.log("useefect item filled")
+      });
+    });
+  }, []);
 
   function getLands() {
+    console.log("getLands init")    
     for (let i = 0; i < 108; i++) {
       for (let j = 0; j < 192; j++) {
-        var rectBounds = [[1 * i, 1 * j], [1 * (i + 1), 1 * (j + 1)]] as LatLngBoundsExpression
-        lands.push(<Rectangle bounds={rectBounds} pathOptions={orange} />)
-        console.log(rectBounds)
+        if (items[i][j] === "1") {
+          var rectBounds = [[1 * i, 1 * j], [1 * (i + 1), 1 * (j + 1)]] as LatLngBoundsExpression
+          localLands.push(<Rectangle bounds={rectBounds} pathOptions={orange} />)
+        }
+        else{
+          
+        }
       }
     }
+    setLands(localLands)
+
+    
     //NO VA
     //return (<>lands</>)
     
@@ -34,8 +68,6 @@ export default function Map() {
     // )
 
     //
-    lands.push(<Rectangle bounds={rectangle} pathOptions={orange} />)
-    lands.push(<Rectangle bounds={maxMapBounds} pathOptions={purpler} />)
 
     return(lands)
    
@@ -63,9 +95,7 @@ export default function Map() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           /> */}
-          {
-            getLands()
-          }
+          {lands}
           <>
           {/* <Rectangle bounds={rectangle} pathOptions={orange} /> */}
           {/* <Rectangle bounds={maxMapBounds} pathOptions={purpler} /> */}
